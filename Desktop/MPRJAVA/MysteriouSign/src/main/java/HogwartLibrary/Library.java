8c4c9fd1dbe574a99f4b992892d1eccffb0eff52
Dominika Malinowska <domin_mal@wp.pl>
2016-11-26 12:27:49 +0100

    library

---
+++ Desktop/MPRJAVA/MysteriouSign/src/main/java/HogwartLibrary/Library.java
@@ -0,0 +1,25 @@
+package HogwartLibrary;
+
+
+public class Library {
+    
+    public void User(){
+        
+        
+    }
+    
+    public void TheBook(){
+        
+        
+    }
+    
+    public void Order(){
+        
+        
+    }
+    
+    public void Author(){
+        
+        
+    }
+}
